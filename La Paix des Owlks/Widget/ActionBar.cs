1095a216e078db203579339bc0b2b37650fb4962
Alexis Huvier <lavapower84@gmail.com>
2024-11-23 21:33:34 +0100

    refactor(actionbar): optimisation color

--- La Paix des Owlks/Widget/ActionBar.cs
+++ La Paix des Owlks/Widget/ActionBar.cs
@@ -6,6 +6,8 @@ namespace La_Paix_des_Owlks.Widget
 {
     internal class ActionBar : SharpEngine.Core.Widget.Widget
     {
+        public Color BackgroundColor { get; set; } = new(200, 200, 200, 100);
+
         public ActionBar(int zLayer = 1000) : base(Vec2.Zero, zLayer)
         {
             AddChild(new ActionBarItem("House", new Vec2(295, 800), "Cout : 1 bois / 1 pierre", new Vec2(250, 30)));
@@ -18,7 +20,7 @@ namespace La_Paix_des_Owlks.Widget
             if(Scene == null)
                 return;
 
-            SERender.DrawRectangle(240, 750, 800, 100, new Color(200, 200, 200, 100), InstructionSource.UI, ZLayer);
+            SERender.DrawRectangle(240, 750, 800, 100, BackgroundColor, InstructionSource.UI, ZLayer-1);
             SERender.DrawRectangleLines(new Rect(245, 755, 90, 90), 5, Color.DarkGray, InstructionSource.UI, ZLayer);
             SERender.DrawRectangleLines(new Rect(345, 755, 90, 90), 5, Color.DarkGray, InstructionSource.UI, ZLayer);
             SERender.DrawRectangleLines(new Rect(445, 755, 90, 90), 5, Color.DarkGray, InstructionSource.UI, ZLayer);
